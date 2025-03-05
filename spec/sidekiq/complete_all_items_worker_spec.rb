9e7fa04f231a76149a9baf50748eb5fa42397e89
Derek Dannevig <ddannevig@derek-dannevig.local>
2025-03-04 23:08:41 -0300

    fix: include factory_bot in rspec

--- spec/sidekiq/complete_all_items_worker_spec.rb
+++ spec/sidekiq/complete_all_items_worker_spec.rb
@@ -5,12 +5,12 @@ RSpec.describe CompleteAllItemsWorker, type: :job do
 
   let(:execute_worker) { subject.perform(todo_list.id) }
 
-  let(:todo_list) { FactoryBot.create(:todo_list) }
-  let(:another_todo_list) { FactoryBot.create(:todo_list) }
-  let(:first_todo_item) { FactoryBot.create(:todo_item, todo_list: todo_list, completed: false) }
-  let(:second_todo_item) { FactoryBot.create(:todo_item, todo_list: todo_list, completed: false) }
-  let(:third_todo_item) { FactoryBot.create(:todo_item, todo_list: todo_list, completed: false) }
-  let(:another_list_item) { FactoryBot.create(:todo_item, todo_list: another_todo_list, completed: false) }
+  let(:todo_list) { create(:todo_list) }
+  let(:another_todo_list) { create(:todo_list) }
+  let(:first_todo_item) { create(:todo_item, todo_list: todo_list, completed: false) }
+  let(:second_todo_item) { create(:todo_item, todo_list: todo_list, completed: false) }
+  let(:third_todo_item) { create(:todo_item, todo_list: todo_list, completed: false) }
+  let(:another_list_item) { create(:todo_item, todo_list: another_todo_list, completed: false) }
 
   it 'completes first pending task' do
     expect { execute_worker }.to change { first_todo_item.reload.completed }.from(false).to(true)
